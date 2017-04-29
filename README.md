4e05816a2bf049855b6295976463ea1cdc8eefa4
Evil Bob <Evil Bob>
2017-04-29 22:30:33 +0300

    update npm info & readme

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Typescript-SimpleEvents
-Very very simple events system for type/java script...
+Very very simple events system for type/java script.
+
+Installation
+------------
+
+In order to install it you can run:
+
+```sh
+$ npm install -g typescript-simpleevents
+```
+
+Usage
+------------
+
+In javascript (node.js, not browser) you can:
+```javascript
+var ev = require("typescript-simpleevents");
+var x = new ev.SimpleEvent();
+x.on(function () { console.log('costel was here.'); });
+x.trigger();
+```
+
+In typescript you can do either:
+```typescript
+import ev = require("typescript-simpleevents");
+
+const x = new ev.SimpleEvent();
+x.on(() => { console.log('costel was here.')});
+x.trigger();
+```
+or
+```typescript
+import { SimpleEvent } from "typescript-simpleevents";
+
+const x = new SimpleEvent();
+x.on(() => { console.log('costel was here.')});
+x.trigger();
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,8 @@
 var gulp = require("gulp");
-var browserify = require("browserify");
 var ts = require("gulp-typescript");
 var sourcemaps = require("gulp-sourcemaps");
-var source = require("vinyl-source-stream");
 var merge = require('merge2');
 var mocha = require('gulp-mocha');
-var notify = require("gulp-notify");
 
 function buildStuff(folder, distPath, generateMap = true) {
     var project = ts.createProject("./" + folder + "/tsconfig.json");
@@ -29,38 +26,15 @@ function buildStuff(folder, distPath, generateMap = true) {
     );
 }
 
-function handleErrors() {
-  var args = Array.prototype.slice.call(arguments);
-  notify.onError({
-    title: "Compile Error",
-    message: "<%= error.message %>"
-  }).apply(this, args);
-  this.emit('end'); // Keep gulp from hanging on this task
-}
-
 gulp.task("build-tests", ["build"], function () {
     return buildStuff("test", "test", false);
 });
 
-gulp.task("build-src", function() {
+gulp.task("build", function() {
     return buildStuff("src", "dist");
 });
 
-gulp.task("browserify", ["build-src"], function () {
-    return browserify({
-        entries: ["./dist/ts-simpleevents.js"]
-    })
-        .bundle()
-        .on('error', handleErrors)
-        .pipe(source("ts-simpleevents-browser.js"))
-        .pipe(gulp.dest("./dist/"));
-});
-
-gulp.task("build", ["build-src", "browserify"], function () {
-    return true;
-});
-
 gulp.task("run-tests", ["build", "build-tests"], function () {
-    return gulp.src('dist/**/*-test.js', { read: false })
+    return gulp.src('test/**/*-test.js', { read: false })
         .pipe(mocha({ reporter: 'spec' }));
 });
